418c0224d2e9695fabb516f305d8bff8a185b7b9
Rafael <markesxd@gmail.com>
2021-09-19 09:55:40 -0300

    add play order

--- script.js
+++ script.js
@@ -32,24 +32,70 @@ function intro() {
     }, 2200);
 }
 
-function startGame(){
-  const buttomOrder = [];
-  for(let i = 0; i < 5; i++){
-    buttomOrder.push(Math.floor(Math.random() * 4));
+function startLevel(number){
+  const order = [];
+  for(let i = 0; i < number; i++){
+    order.push(Math.floor(Math.random() * 4));
   }
-  return buttomOrder;
+  return order;
+}
+
+async function playLevel(order){
+  console.log(order)
+  for(color of order){
+    await flash(color);
+    await delay(()=> {}, 100);
+  }
+}
+
+function flash(color){
+  switch (color){
+    case 0:
+      yellow.classList.add('yellow-selected');
+      yellow.classList.remove('yellow');
+      return delay(() => {
+        yellow.classList.add('yellow');
+        yellow.classList.remove('yellow-selected');
+      }, 300);
+    case 1:
+      red.classList.add('red-selected');
+      red.classList.remove('red');
+      return delay(() => {
+        red.classList.add('red');
+        red.classList.remove('red-selected');
+      }, 300);
+    case 2:
+      green.classList.add('green-selected');
+      green.classList.remove('green');
+      return delay(() => {
+        green.classList.add('green');
+        green.classList.remove('green-selected');
+      }, 300);
+    case 3:
+      blue.classList.add('blue-selected');
+      blue.classList.remove('blue');
+      return delay(() => {
+        blue.classList.add('blue');
+        blue.classList.remove('blue-selected');
+      }, 300);
+  }
 }
 
 function delay(func, time){
   return new Promise((resolve) => {
-    setTimeout(()=>{func(); resolve()}, time);
+    setTimeout(() => {func(); resolve()}, time);
   })
 }
 
 async function game(){
  await intro();
- const buttomOrder = startGame();
- 
+ // yellow.onclick = () => {flash(0)}
+ // red.onclick = () => {flash(1)}
+ // green.onclick = () => {flash(2)}
+ // blue.onclick = () => {flash(3)}
+ const order = startLevel(5);
+ playLevel(order);
+
 }
 
 game();
